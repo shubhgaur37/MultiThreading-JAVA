d97cff90ea32529d87212fc031fbf28d25229068
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-14 09:17:19 +0530

    Lambda Expressions Deep Dive

--- src/Thread_Lambda_Expression.java
+++ src/Thread_Lambda_Expression.java
@@ -1,3 +1,5 @@
+
+
 public class Thread_Lambda_Expression {
 
     public static void main(String[] args) {
@@ -14,7 +16,35 @@ public class Thread_Lambda_Expression {
 
         myThread.start();
         runnableThread.start();
+
+//        Using Lambda Function with a functional interface type
+//        because there is a single argument in the method, we need not use () to write arguments
+
+//        Student student = name -> {
+//           return name + " is a boy";
+//        };
+
+        // Also, since there is a single statement in function definition, we can remove curly braces and return statement
+        Student student = name -> name + " is a boy";
+
+        System.out.println(student.getNameDesc("RAM"));
+
+        // More than one argument, so needed to wrap them in circular brackets
+        Person person =  (name,age) -> {
+            System.out.println("Name: "+ name + " | Age: " + age);
+        };
+
+        person.printNameAge("Shubh",26);
+
     }
+}
+
+interface Student{
+    String getNameDesc(String name);
+}
+
+interface Person{
+    void printNameAge(String name, int age);
 }
 
 
