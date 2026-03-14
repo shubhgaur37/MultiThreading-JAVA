fc02b5a5155fd634e0de42a6c48ebe46346ac378
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-14 10:40:44 +0530

    Thread Pooling

--- src/ExecutorsFramework/Factorial_Example_Multithreading.java
+++ src/ExecutorsFramework/Factorial_Example_Multithreading.java
@@ -2,17 +2,62 @@ package ExecutorsFramework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Factorial_Example_Multithreading {
 
     public static void main(String[] args) {
-
+        printFactorialThreadPool();
 //        printFactorialSingleThreaded();
 //        printFactorialMultiThreaded();
     }
     public static void printFactorialThreadPool(){
+        System.out.println("MULTI THREADED FACTORIAL USING EXECUTORS FRAMEWORK");
+//        Initialise a thread pool of 9 threads
+        ExecutorService executorService = Executors.newFixedThreadPool(9);
+        long startTime = System.currentTimeMillis();
+//        for storing threads
+        List<Thread> threads = new ArrayList<>();
 
+        for(int num = 1; num < 10; num++){
+            int finalNum = num;
+            // throws error if num is directly used in lambd
+            // because num can change, and variable used in lambda expression should be final
+
+            // submit takes in a runnable, which is passed as a lambda
+            executorService.submit(() ->{
+                System.out.println("Num: " + finalNum + " | Factorial: " + factorial(finalNum));
+            });
+        }
+        // used to close threads when the work is fully completed, if this is not used
+        // then the threads remain active and application keeps running(needed to manually stop)
+        // and usable for future tasks
+        executorService.shutdown();
+        // after shutdown new tasks cannot be submitted to the thread pool
+        // runtime exception
+
+        // moreover main thread does not wait for executor to shutdown, it executes by itself
+        long endTime = System.currentTimeMillis();
+//        Output
+//        Time Taken: 1
+//############################
+//        Num: 2 | Factorial: 2
+//        Num: 3 | Factorial: 6
+
+        // we want all tasks to complete before running the below statement which can be achieved using awaitTermination
+        // always used after a shutdown method invokation
+        try {
+            // waits for maximum of 100 seconds(timeout) for all tasks to complete, or current thread interruption and then resumes
+            executorService.awaitTermination(100, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Time Taken: " + (endTime - startTime));
+        System.out.println("############################");
     }
+
     public static void printFactorialSingleThreaded(){
         System.out.println("SINGLE THREADED FACTORIAL");
         long startTime = System.currentTimeMillis(); // milliseconds elapsed from 01 JAN 1970 00:00 UTC
